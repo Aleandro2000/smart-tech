9addeec5742af2c9f5763036667dd95a6ba2d5d1
dacosoftsolution <carmicialexandru@yahoo.com>
2022-05-04 20:20:03 +0300

    hard-and-soft

---
+++ lib/src/login/login.dart
@@ -0,0 +1,73 @@
+import "package:flutter/material.dart";
+import 'package:hard_and_soft_mobile/src/register/register.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: const Image(image: AssetImage('assets/logo.png')),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.fromLTRB(50, 0, 50, 12.5),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Username',
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.fromLTRB(50, 0, 50, 12.5),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Password',
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'LOGIN',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Register()),
+                );
+              },
+              child: const Text(
+                "Don't you have an account?",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

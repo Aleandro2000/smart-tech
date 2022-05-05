533d6d86887a196bd6d49c042cdab5e10cbd51df
dacosoftsolution <carmicialexandru@yahoo.com>
2022-05-05 17:56:02 +0300

    hard-and-soft

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Smart Tech',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),

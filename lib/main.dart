af0b8963aa1def6623d40988e489e87c0c586453
dacosoftsolution <carmicialexandru@yahoo.com>
2022-05-06 19:37:46 +0300

    hard-and-soft

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hard_and_soft_mobile/src/login/login.dart';
 import 'package:hard_and_soft_mobile/src/register/register.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hard_and_soft_mobile/src/splashScreen/splashScreen.dart';
 
 Future main() async {
-  await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 

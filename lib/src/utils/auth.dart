f60e44daab7cce159d7b742ee52bec75c1af361b
dacosoftsolution <carmicialexandru@yahoo.com>
2022-05-09 16:08:53 +0300

    hard-and-soft

--- lib/src/utils/auth.dart
+++ lib/src/utils/auth.dart
@@ -1,29 +1,55 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
-Future<bool> registerAuth(String email, String password) async {
-  if (email.isNotEmpty && password.isNotEmpty) {
-    await FirebaseAuth.instance
-        .signInWithEmailAndPassword(email: email, password: password);
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
-      await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: email, password: password);
+Future<bool> checkIfEmailInUse(String emailAddress) async {
+  try {
+    final list =
+        await FirebaseAuth.instance.fetchSignInMethodsForEmail(emailAddress);
+
+    if (list.isNotEmpty) {
       return true;
     } else {
-      logoutAuth();
+      return false;
     }
+  } catch (error) {
+    return true;
   }
-  return false;
+}
+
+Future<bool> registerAuth(String email, String password) async {
+  try {
+    if (email.isNotEmpty &&
+        password.isNotEmpty &&
+        !await checkIfEmailInUse(email)) {
+      User? user = (await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(email: email, password: password))
+          .user;
+      if (user != null) {
+        user.sendEmailVerification();
+        return true;
+      }
+      return false;
+    }
+    return false;
+  } on PlatformException {
+    return false;
+  } catch (err) {
+    return false;
+  }
 }
 
 Future<bool> loginAuth(String email, String password) async {
-  if (email.isNotEmpty && password.isNotEmpty) {
-    await FirebaseAuth.instance
-        .signInWithEmailAndPassword(email: email, password: password);
-    User? user = FirebaseAuth.instance.currentUser;
-    return user != null;
+  try {
+    if (email.isNotEmpty && password.isNotEmpty) {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+      User? user = FirebaseAuth.instance.currentUser;
+      return user != null;
+    }
+    return false;
+  } catch (err) {
+    return false;
   }
-  return false;
 }
 
 User? getSession(bool debugLog) {
@@ -40,5 +66,7 @@ User? getSession(bool debugLog) {
 }
 
 void logoutAuth() async {
-  await FirebaseAuth.instance.signOut();
+  if (FirebaseAuth.instance.currentUser != null) {
+    await FirebaseAuth.instance.signOut();
+  }
 }

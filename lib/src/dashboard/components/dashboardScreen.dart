1b18fd96b8b2ce5dfc54046e5c0d998f997e6836
dacosoftsolution <carmicialexandru@yahoo.com>
2022-05-10 00:33:11 +0300

    hard-and-soft

--- lib/src/dashboard/components/dashboardScreen.dart
+++ lib/src/dashboard/components/dashboardScreen.dart
@@ -1,7 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:hard_and_soft_mobile/src/login/login.dart';
-import 'package:hard_and_soft_mobile/src/templates/appBarTemplate.dart';
-import 'package:hard_and_soft_mobile/src/utils/auth.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -11,20 +8,6 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
-  void logout(context) {
-    logoutAuth();
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const Login()),
-    );
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('Successfully logged out ;)'),
-        backgroundColor: Colors.green,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column();
---
+++ lib/src/dashboard/components/pannelScreens.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class PannelScreen extends StatefulWidget {
+  const PannelScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PannelScreen> createState() => _PannelScreenState();
+}
+
+class _PannelScreenState extends State<PannelScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}

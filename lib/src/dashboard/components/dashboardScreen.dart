adc322c582394d4ad2bae42fc330f3afd2fa04fa
dacosoftsolution <carmicialexandru@yahoo.com>
2022-05-11 21:10:29 +0300

    hard-and-soft

--- lib/src/dashboard/components/dashboardScreen.dart
+++ lib/src/dashboard/components/dashboardScreen.dart
@@ -13,26 +13,30 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: AspectRatio(
-            aspectRatio: 16 / 9,
-            child: DChartLine(
-              data: const [
-                {
-                  'id': 'Line',
-                  'data': [
-                    {'domain': 0, 'measure': 2.5},
-                    {'domain': 2, 'measure': 4},
-                    {'domain': 3, 'measure': 6},
-                    {'domain': 4, 'measure': 1},
-                  ],
-                },
-              ],
-              includePoints: true,
-              lineColor: (lineData, index, id) => Colors.amber,
+        Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: AspectRatio(
+              aspectRatio: 16 / 9,
+              child: DChartLine(
+                data: const [
+                  {
+                    'id': 'Line',
+                    'data': [
+                      {'domain': 0, 'measure': 2.5},
+                      {'domain': 2, 'measure': 4},
+                      {'domain': 3, 'measure': 6},
+                      {'domain': 4, 'measure': 1},
+                    ],
+                  },
+                ],
+                includePoints: true,
+                lineColor: (lineData, index, id) => Colors.amber,
+              ),
             ),
           ),
+          elevation: 8,
+          margin: EdgeInsets.all(20),
         ),
       ],
     );

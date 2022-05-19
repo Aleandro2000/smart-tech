93608518a4d29a501bd60b1be8382d370c58a13e
dacosoftsolution <carmicialexandru@yahoo.com>
2022-05-20 01:06:02 +0300

    hard-and-soft

--- lib/src/pannel/commandPannel.dart
+++ lib/src/pannel/commandPannel.dart
@@ -22,38 +22,39 @@ class _CommandPannelState extends State<CommandPannel> {
         title: "Command Pannel",
       ),
       body: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                margin: const EdgeInsets.fromLTRB(50, 50, 50, 12.5),
-                child: TextFormField(
-                  controller: input,
-                  validator: (value) => requiredValidation(value!),
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Input',
+        key: _formKey,
+        child: ListView(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.fromLTRB(50, 50, 50, 12.5),
+              child: TextFormField(
+                controller: input,
+                validator: (value) => requiredValidation(value!),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Input',
+                ),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  child: const Text(
+                    'Submit',
+                    style: TextStyle(fontSize: 18),
                   ),
                 ),
               ),
-              Container(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {}
-                    },
-                    child: const Text(
-                      'Submit',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
